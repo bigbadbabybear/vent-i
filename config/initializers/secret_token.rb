083a59a06a2bca747600066f6e5cdd98762625c2
ErwinBatucan <erwinbatucan@gmail.com>
2012-08-08 15:26:18 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VentOut::Application.config.secret_token = '0424e0587a4d94f3875457dd86a61880536ec5fb6c370a268bf462f79aa9dc23bd595cb8cbdf2129224f827f72c936d3b006f628f80308252b09258da7b6abe1'
